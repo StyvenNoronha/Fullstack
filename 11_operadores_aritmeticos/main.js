070a79461ff0e419ca71e6c448113348c50a9d6b
styven <styvenn16@gmail.com>
2024-11-08 08:07:26 -0300

    aula de ontem de js

---
+++ 11_operadores_aritmeticos/main.js
@@ -0,0 +1,7 @@
+//operadores aritmeticos
+console.log("soma",14+2)
+console.log("multiplicação",14*2)
+console.log("subtração",14-2)
+console.log("divisão",14/2)
+console.log("resto da divisão",14%3)
+console.log("exponencial",14 ** 3)
---
+++ 12_incremento_decremento/main.js
@@ -0,0 +1,13 @@
+let number = 10 
+let numero = 10
+number ++ 
+numero --
+
+console.log(number)
+console.log(numero)
+
+number+= 1
+numero-=1
+
+console.log(number)
+console.log(numero)
