6a30489353668ffc62a8fc42a8d8c8098d881851
styven <styvenn16@gmail.com>
2024-11-08 11:12:56 -0300

    aula 17 e 18

---
+++ 17_switch/main.js
@@ -0,0 +1,15 @@
+let option = 4;
+
+switch (option) {
+  case 1:
+    console.log("opção 1 ");
+    break;
+  case 2:
+    console.log("opção 2 ");
+    break;
+  case 3:
+    console.log("opção 3 ");
+    break;
+  default:
+    console.log("opção invalidade")  
+}
---
+++ 18_try_catch_finally/main.js
@@ -0,0 +1,5 @@
+try {
+  console.log(aasasas);
+} catch (error) {
+  console.log("erro ");
+}
