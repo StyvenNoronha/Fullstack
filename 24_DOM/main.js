2eabdd943b9716b24f8341c500bc087d4691e5e8
styven <styvenn16@gmail.com>
2024-11-09 20:07:09 -0300

    aprendendo DOM

---
+++ 24_DOM/main.js
@@ -0,0 +1,11 @@
+//acessa o elemento pelo o seletor ID
+const guest = document.querySelector("#guest-1")
+console.log(guest)
+
+//acessa o elemento pela classe
+const guestClass = document.querySelector(".guest")
+console.log(guestClass)
+
+
+//acessa todos os elementos da classe 
+const guestAllSelector = document.querySelectorAll(".guest")
---
+++ 24_DOM/scripts.js
@@ -0,0 +1,25 @@
+/*
+//selecionar o elemento com o ID guest-2
+const guest = document.getElementById("guest-2");
+console.log(guest);
+
+//obter o title da pagina
+const title = document.title;
+console.log(title);
+
+//acessar o elemento
+console.dir(guest);
+
+//acessar elemento com class
+const guestList = document.getElementsByClassName("guest");
+console.log(guestList);
+
+//exibir o primeiro elemento da lista
+console.log(guestList.item(0));
+console.log(guestList[0]);
+
+//selecionar lista de elementos pela tag
+const guestList2 = document.getElementsByTagName("li");
+console.log(guestList2);
+*/
+
