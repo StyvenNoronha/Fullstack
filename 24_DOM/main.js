9d85c1cdcbdc39026096b71faa1d4d6cef5b627a
styven <styvenn16@gmail.com>
2024-11-10 16:38:25 -0300

    DOM part 1

--- 24_DOM/main.js
+++ 24_DOM/main.js
@@ -1,3 +1,4 @@
+/*
 //acessa o elemento pelo o seletor ID
 const guest = document.querySelector("#guest-1")
 console.log(guest)
@@ -6,6 +7,50 @@ console.log(guest)
 const guestClass = document.querySelector(".guest")
 console.log(guestClass)
 
-
 //acessa todos os elementos da classe 
 const guestAllSelector = document.querySelectorAll(".guest")
+
+const guest = document.querySelector("#guest-1 span")
+
+//retorna o conteúdo como texto
+console.log(guest.textContent)
+
+//escreve dentro do conteúdo
+guest.textContent = "Alberto"
+
+const guest = document.querySelector("#guest-1")
+
+console.log(guest.textContent) //Retorna o conteúdo visível e oculto
+console.log(guest.innerText) //Retorna somente o conteúdo visível
+console.log(guest.innerHTML) //Retorna o HTML como texto
+
+const input = document.getElementById("name")
+//por meio do js adiciona uma nova classe no elemento
+input.classList.add("input-error")
+
+//se não tiver a classe adiciona. Se tem, remove
+input.classList.toggle("input-error")
+
+const button = document.querySelector("button")
+//Modificar as propriedades CSS do elemento
+button.style.backgroundColor = "blue"
+*/
+//seleciona o ul
+const guests = document.querySelector("ul")
+//seleciona o li
+const newGuest = document.createElement("li")
+//adiciona uma class
+newGuest.classList.add("guest")
+//criar um elemento span
+const guestName = document.createElement("span")
+//adiciona um texto
+guestName.textContent = "cristina"
+
+//adiciona apos o ultimo filho
+newGuest.append(guestName)
+
+//adiciona  antes do primeiro filho
+//newGuest.prepend(guestName)
+
+//adiciona no HTML
+guests.append(newGuest)
